e116f4516ff9479e01a087c617d59bbbf5aafe2f
Davide Di Pol <31983058+DavideDiPol@users.noreply.github.com>
2018-02-10 23:34:52 +0100

    Tris

--- Tris/app/src/main/java/com/example/davide/tris/MainActivity.java
+++ Tris/app/src/main/java/com/example/davide/tris/MainActivity.java
@@ -1,23 +1,108 @@
 package com.example.davide.tris;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Observable;
+import java.util.Observer;
+
+public class MainActivity extends AppCompatActivity implements Observer {
+
+
+    char Player = 'X';//viene impostato "X" come giocatore
+    boolean isX = true; // booleano che indica se il giocatore coerrente è X o meno, e gestisce i turno
+    Tabella t = new Tabella();
+    int[] lastMove = new int[2];
+    Button lastClicked;
+    boolean playable = true;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        char [ ] [ ] gioco = new char [3] [3];
-        for (int i = 0; i < 3; i++){
-            for(int j = 0; j < 3; j++){
-                gioco [i] [j] = 'a'; //nel caso in cui il valore interno sia 'a' la casella è segnata come vuota
-            }
-        }
-        char Player = 'x';//viene impostato "X" come giocatore
-        boolean isX = true; // booleano che indica se il giocatore coerrente è X o meno, e gestisce i turni
+        ((TextView) findViewById(R.id.msg)).setText("Tocca a " + Player);
+        t.addObserver(this);
     }
 
+
+    public void changeTurn() {
+        Player = (Player == 'X') ? 'O' : 'X';
+        isX = !isX;
+    }
+
+
+    @Override
+    public void update(Observable o, Object arg) {
+        lastClicked.setText(Player + "");
+        playable = t.checkTable();
+        if (playable) {
+            ((TextView) findViewById(R.id.msg)).setText("Tocca a " + Player);
+        } else {
+            ((TextView) findViewById(R.id.msg)).setText("Ha vinto " + Player);
+        }
+        this.changeTurn();
+
+    }
+
+    public void click(View v) {
+        lastClicked = (Button) v;
+        switch (v.getId()) {
+            case R.id.btnTL:
+                lastMove[0] = 0;
+                lastMove[1] = 0;
+                t.update(Player, lastMove, playable);
+                break;
+            case R.id.btnTC:
+                lastMove[0] = 0;
+                lastMove[1] = 1;
+                t.update(Player, lastMove, playable);
+                break;
+            case R.id.btnTR:
+                lastMove[0] = 0;
+                lastMove[1] = 2;
+                t.update(Player, lastMove, playable);
+                break;
+            case R.id.btnML:
+                lastMove[0] = 1;
+                lastMove[1] = 0;
+                t.update(Player, lastMove, playable);
+                break;
+            case R.id.btnMC:
+                lastMove[0] = 1;
+                lastMove[1] = 1;
+                t.update(Player, lastMove, playable);
+                break;
+            case R.id.btnMR:
+                lastMove[0] = 1;
+                lastMove[1] = 2;
+                t.update(Player, lastMove, playable);
+                break;
+            case R.id.btnBL:
+                lastMove[0] = 2;
+                lastMove[1] = 0;
+                t.update(Player, lastMove, playable);
+                break;
+            case R.id.btnBC:
+                lastMove[0] = 2;
+                lastMove[1] = 1;
+                t.update(Player, lastMove, playable);
+                break;
+            case R.id.btnBR:
+                lastMove[0] = 2;
+                lastMove[1] = 2;
+                t.update(Player, lastMove, playable);
+                break;
+            case R.id.btnRestart:
+                Intent intent = getIntent();
+                finish();
+                startActivity(intent);
+                break;
+        }
+    }
 
 }
